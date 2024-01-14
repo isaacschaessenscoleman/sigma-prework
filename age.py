2ebb1c82075560164c3ce76b78d757c0c1550526
Isaac Coleman <isaaccoleman58@gmail.com>
2024-01-14 18:22:32 +0000

    submitting pre work scripts

---
+++ age.py
@@ -0,0 +1,15 @@
+import datetime
+
+date = input('Enter a date in the following format DD-MM-YEAR: ').split('-')
+day, month, year = int(date[0]), int(date[1]), int(date[2])
+inputted_date = datetime.date(year, month, day)
+
+todays_date = datetime.datetime.today().date()
+
+age = (todays_date - inputted_date).days / 365.25
+
+age_format = "%.1f" % age
+
+print(f"It's been {age_format} years since that date!")
+
+
---
+++ maxmin.py
@@ -0,0 +1,26 @@
+list = input('Enter a list of numbers separated by a space').split(' ')
+num_list = [int(i) for i in list]
+
+def max(list):
+
+    max = float('-inf')
+    for num in list:
+        if num > max:
+            max = num
+
+    return max
+
+def min(list):
+
+    min = float('inf')
+    for num in list:
+        if num < min:
+            min = num
+
+    return min
+
+
+print(f" Maximum: {max(num_list)} \n Minimum: {min(num_list)}")
+
+
+
